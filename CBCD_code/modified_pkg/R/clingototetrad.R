386c98d87fd3c647727be2156729021b519349ec
Alex Rix <alexanderjrix@gmail.com>
2017-10-04 15:28:12 -0500

    update git ignore and add clingototetrad.R'

---
+++ CBCD_code/modified_pkg/R/clingototetrad.R
@@ -0,0 +1,30 @@
+
+run <- function(file, schedule, p) {
+  runData("../AUD_DATA/NESARC_jv_sums_mtz_nzr (1).csv", e = rep(0,8), encode = "new_wmaxsat.pl", test = "BIC", p = p, schedule = schedule, sep = "," ,header = T)
+  foo <-parse_solution(encode="new_wmaxsat.pl", sol_file = "../tmp/pipeline.ind.clingo")
+  nm <- c("sum_DTC","sum_AUD","sum_MDD","sum_DYS","sum_GAD","sum_SOC","sum_PAN","sum_PTSD")
+  write("Graph Nodes:", file=file, append = F)
+  cat(nm, file=file, append = T, sep = ",")
+  write("\n", file=file, append = T)
+  write("Graph Edges:", file=file, append = T)
+  e=1
+  for(i in 1:8) {
+    for(j in 1:8) {
+      if (foo$G[i,j]) {
+        write(sprintf("%i. %s --> %s", e, nm[j], nm[i]), file=file, append = T)
+        e=e+1
+      }
+    }
+  }
+  for(i in 1:8) {
+    for(j in 1:8) {
+      if (foo$Ge[i,j]) {
+        foo$Ge[j,i] = 0
+        write(sprintf("%i. %s o-o %s", e, nm[j], nm[i]), file=file, append = T)
+        e=e+1
+      }
+    }
+  }
+
+
+}
